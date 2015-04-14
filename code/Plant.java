2c0cc32e6896537b522e33716c7080eb56a90740
Nathan <nburow@purdue.edu>
2015-04-14 15:50:54 -0400

    allow power plant to have user specified stddev

--- code/Plant.java
+++ code/Plant.java
@@ -1,17 +1,25 @@
 
 import java.util.Random;
 
+/*Simulates all of the renewable power sources, which
+ * when lumped together produce a normally distributed
+ * amount of power
+ * */
+
 public class Plant {
 	private int mean;
+	private int stddev;
+
 	private Random r;
 
-	Plant(int mean){
+	/*Using a fixed seed for now for debugging */
+	Plant(int mean, int stddev){
 		this.mean = mean;
+		this.stddev = stddev;
 		r = new Random(123456789);
 	}
 
 	public int produce(){
-		//mu = mean, stddev = 1
-		return r.nextGaussian()+mean;  
+		return r.nextGaussian()*stddev+mean;  
 	}
 }
