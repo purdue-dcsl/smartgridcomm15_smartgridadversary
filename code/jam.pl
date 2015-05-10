b63920fbbb5796c3904657f71ee125190c86a66e
Nathan <nburow@purdue.edu>
2015-05-10 10:24:54 -0400

    data runs

---
+++ code/jam.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl -w
+
+$cmd = "javac *.java";
+system($cmd);
+$time = 100;
+@seeds = (123456789, 987654321, 147258369, 369258147, 789456123, 
+    456123789, 258369147, 369147258, 159264837, 849516237);
+
+$att = "jam";
+@percentage = (5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
+
+$cmd = "printf \"%10s,%10s,%10s,%10s\n\" percent baseline before after > $att" . "_parallel.csv";
+system($cmd);
+
+foreach $per (@percentage) {
+  foreach $seed (@seeds){
+    $cmd = "java Simulator " . $time . " " . $seed . " " . $att . " " . $per . " >> " . $att . "_parallel.csv";
+    print $cmd . "\n";
+    system($cmd);
+  }
+}
