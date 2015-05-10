f1557190268178ee4f2b48a26d05680b08697524
Nathan <nburow@purdue.edu>
2015-05-09 23:47:09 -0400

    scripts to quarter data time

--- code/run_jam.pl
+++ code/run_jam.pl
@@ -9,11 +9,11 @@ $time = 100;
 $att = "jam";
 @percentage = (5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
 
-`printf "%10s,%10s,%10s,%10s\n" percent baseline before after > $att.csv`  
+`printf "%10s,%10s,%10s,%10s\n" percent baseline before after > $att_parallel.csv`  
 
 foreach my $per (@percentage) {
   foreach my $seed (@seeds){
-    $cmd = "java Simulator " . $time . " " . $seed . " " . $att . " " . $per . " >> " . $att . ".csv";
+    $cmd = "java Simulator " . $time . " " . $seed . " " . $att . " " . $per . " >> " . $att . "_parallel.csv";
     print $cmd . "\n";
     system($cmd);
   }
